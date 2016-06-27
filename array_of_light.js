c181464a8953f9c998f5f98ba40a60953e97b198
William Young <william@werl.me>
2016-06-27 11:41:51 -0700

    Create array of light.
    Generates an array with every number including 0 and the given number,
    up to the number.

---
+++ array_of_light.js
@@ -0,0 +1,10 @@
+function arrayOfLight(x) {
+  var arr = [];
+  for(var i = 0; i <= x; i++) {
+    arr.push(i);
+  }
+  return arr;
+}
+
+console.log(arrayOfLight(10));
+console.log(arrayOfLight(25));
